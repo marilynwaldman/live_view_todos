5bb2cf483333f380ecb570b20c0b46de8445a5d4
MarilynWaldman <mdwaldman22@hotmail.com>
2020-02-04 14:38:59 -0700

    added db schema for todos, then rendered list of todos

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -1,16 +1,16 @@
 defmodule LiveViewTodosWeb.TodoLive do
   use Phoenix.LiveView
 
-  alias LiveViewTodosWeb.TodoView
   alias LiveViewTodos.Todos
-
+  alias LiveViewTodosWeb.TodoView
 
   def mount(_session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, todos:  Todos.list_todos())}
+
   end
 
   def render(assigns) do
-    ~L"Rendering LiveView"
+    TodoView.render("todos.html", assigns)
   end
 end
 
