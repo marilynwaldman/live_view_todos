9e0286eae3e48e2b4dc821775e03cd7c5a55c497
MarilynWaldman <mdwaldman22@hotmail.com>
2020-02-04 14:41:16 -0700

    added the ***add*** key

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -5,8 +5,17 @@ defmodule LiveViewTodosWeb.TodoLive do
   alias LiveViewTodosWeb.TodoView
 
   def mount(_session, socket) do
-    {:ok, assign(socket, todos:  Todos.list_todos())}
+    {:ok, fetch(socket)}
+  end
 
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
   end
 
   def render(assigns) do
